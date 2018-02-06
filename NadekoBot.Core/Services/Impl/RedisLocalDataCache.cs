b389fdacb45a15140465b25fb5dc9e175a67b7ba
noroinohanako <komajiros@gmail.com>
2018-02-06 01:46:23 -0500

    semi

--- NadekoBot.Core/Services/Impl/RedisLocalDataCache.cs
+++ NadekoBot.Core/Services/Impl/RedisLocalDataCache.cs
@@ -23,7 +23,7 @@ namespace NadekoBot.Core.Services.Impl
         private const string pokemonListFile = "data/pokemon/pokemon_list7.json";
         private const string pokemonMapPath = "data/pokemon/name-id_map4.json";
         private const string questionsFile = "data/trivia_questions.json";
-        private const string youkaiTrivia = "data/youkai_trivia.json"
+        private const string youkaiTrivia = "data/youkai_trivia.json";ß
 
         public IReadOnlyDictionary<string, SearchPokemon> Pokemons
         {
